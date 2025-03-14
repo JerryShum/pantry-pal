2dbb524c4f9985a3f3e9e8446f375dd3d56a7a1b
JerryShum <jerryshum2636@gmail.com>
2025-03-14 14:58:18 -0400

    feat: getting the server to serve statically built frontend

--- server.ts
+++ server.ts
@@ -1,10 +1,14 @@
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
+import { serveStatic } from 'hono/bun';
 
 const server = new Hono();
 
 // Middleware that logs all requests
 server.use('/*', logger());
+
+// Middleware that serves static files
+server.use('/*', serveStatic({ root: './frontend/dist' }));
 
 server.get('/test', (c) => {
    return c.json({
