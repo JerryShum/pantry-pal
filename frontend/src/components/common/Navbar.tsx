4cf33ad4b0715c87af71415a0bb547ec883bccc0
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-03-15 09:34:05 -0400

    navbar styling

--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -1,7 +1,7 @@
 import { Link } from "@tanstack/react-router";
 export default function Navbar() {
   return (
-    <nav className="flex gap-2 p-2">
+    <nav className="flex gap-2 p-2 px-60">
       <Link to="/" className="[&.active]:font-bold">
         Home
       </Link>{" "}
