7152dce2cee33b10c2f2173ec394cc4be8cdfc38
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-03-14 21:06:21 -0400

    feat: create recipe card to work with recipe type

--- frontend/src/components/common/RecipeCard.tsx
+++ frontend/src/components/common/RecipeCard.tsx
@@ -2,16 +2,24 @@ import React from "react";
 import { Card, CardContent, CardHeader } from "../UI/card";
 import { Recipe } from "../../../../schema";
 
-export default function RecipeCard({ recipe }: Recipe) {
+export default function RecipeCard({
+  title,
+  imageUrl,
+  ingredients,
+  description,
+  instructions,
+}: Recipe) {
   return (
     <Card>
       <CardHeader>
-        <h2>{recipe.title}</h2>
+        <h2>{title}</h2>
       </CardHeader>
       <CardContent>
         <div className="flex flex-col gap-2">
-          <img src={recipe.imageUrl} alt={recipe.title} />
-          <p>Ingredients: {recipe.ingredients.join(", ")}</p>
+          <img src={imageUrl ?? ""} alt={title} />
+          <p>Ingredients: {ingredients}</p>
+          <p>{description}</p>
+          <p>{instructions}</p>
         </div>
       </CardContent>
     </Card>
